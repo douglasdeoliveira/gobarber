a28593ba37411d73303a13a839cd55e9336465c8
Douglas Oliveira <oliveira.douglas@outlook.com>
2020-05-03 23:16:24 -0300

    [mobile] styles: finish SignIn screen layout

--- mobile/src/screens/SignIn/styles.ts
+++ mobile/src/screens/SignIn/styles.ts
@@ -1,3 +1,5 @@
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+
 import styled from 'styled-components/native';
 
 import colors from '../../styles/colors';
@@ -6,6 +8,7 @@ export const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+  padding: 0 30px;
 `;
 
 export const Title = styled.Text`
@@ -13,4 +16,35 @@ export const Title = styled.Text`
   color: ${colors.lightGray};
   font-family: 'RobotoSlab-SemiBold';
   margin: 64px 0 24px;
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  font-size: 16px;
+  color: ${colors.lightGray};
+  font-family: 'RobotoSlab-Regular';
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  left: 0%;
+  right: 0;
+  bottom: 0;
+  background: ${colors.gray};
+  border-top-width: 1px;
+  border-color: ${colors.darkGray};
+  padding: 16px 0 ${16 + getBottomSpace()}px;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  font-size: 18px;
+  color: ${colors.orange};
+  font-family: 'RobotoSlab-Regular';
+  margin-left: 16px;
 `;
