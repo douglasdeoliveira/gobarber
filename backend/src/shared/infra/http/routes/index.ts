e50c136352fb4688fd831852b031727f63f11fe5
Douglas Oliveira <oliveira.douglas@outlook.com>
2020-05-18 23:15:27 -0300

    [backend] tests: finish tests to forgot password email

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import appointments from '@modules/appointments/infra/http/routes/appointments.routes';
+import password from '@modules/users/infra/http/routes/password.routes';
 import sessions from '@modules/users/infra/http/routes/sessions.routes';
 import users from '@modules/users/infra/http/routes/users.routes';
 
@@ -9,5 +10,6 @@ const routes = Router();
 routes.use('/sessions', sessions);
 routes.use('/appointments', appointments);
 routes.use('/users', users);
+routes.use('/password', password);
 
 export default routes;
