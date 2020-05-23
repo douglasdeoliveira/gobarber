df3793302d8563888075d48523a78db39bf5eb3c
Douglas Oliveira <oliveira.douglas@outlook.com>
2020-05-23 18:49:10 -0300

    [backend] feat: add controllers and routes to list days and hours availables

--- backend/src/modules/appointments/infra/http/routes/providers.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,13 +2,25 @@ import { Router } from 'express';
 
 import ensureAthenticated from '@modules/users/infra/http/middlewares/ensureAthenticated';
 
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController';
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController';
 import ProvidersController from '../controllers/ProvidersController';
 
 const routes = Router();
 const providersController = new ProvidersController();
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController();
+const providerDayAvailabilityController = new ProviderDayAvailabilityController();
 
 routes.use(ensureAthenticated);
 
 routes.get('/', providersController.index);
+routes.get(
+  '/:provider_id/month-availability',
+  providerMonthAvailabilityController.index,
+);
+routes.get(
+  '/:provider_id/day-availability',
+  providerDayAvailabilityController.index,
+);
 
 export default routes;
