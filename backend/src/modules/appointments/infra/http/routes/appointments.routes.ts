17b7d0505074eccb139d4f8dc978feb8bb56c70e
Douglas Oliveira <oliveira.douglas@outlook.com>
2020-05-24 00:20:29 -0300

    [backend] feat: add list appointments provider

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -3,18 +3,15 @@ import { Router } from 'express';
 import ensureAthenticated from '@modules/users/infra/http/middlewares/ensureAthenticated';
 
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const routes = Router();
 const appointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 routes.use(ensureAthenticated);
 
-// routes.get('/', async (request, response) => {
-//   const appoitments = await appointmentsRepository.find();
-
-//   response.json(appoitments);
-// });
-
 routes.post('/', appointmentsController.create);
+routes.get('/me', providerAppointmentsController.index);
 
 export default routes;
