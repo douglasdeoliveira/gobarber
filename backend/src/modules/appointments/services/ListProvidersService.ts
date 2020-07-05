b99df5452fdfbd5cea0b9475e5ff3cea99fef676
Douglas de Oliveira <oliveira.douglas@outlook.com>
2020-07-05 19:30:13 -0300

    [backend] fix: add missing classToClass

--- backend/src/modules/appointments/services/ListProvidersService.ts
+++ backend/src/modules/appointments/services/ListProvidersService.ts
@@ -1,3 +1,4 @@
+import { classToClass } from 'class-transformer';
 import { injectable, inject } from 'tsyringe';
 
 import User from '@modules/users/infra/typeorm/entities/User';
@@ -28,7 +29,7 @@ class ListProvidersService {
         except_user_id: user_id,
       });
 
-      await this.cacheProvider.save(cacheKey, users);
+      await this.cacheProvider.save(cacheKey, classToClass(users));
     }
 
     return users;
