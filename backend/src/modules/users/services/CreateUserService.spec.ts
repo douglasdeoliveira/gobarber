d176232c1c4cdf6d2a10090fe2cedce02d82aa8c
Douglas de Oliveira <oliveira.douglas@outlook.com>
2021-05-23 14:17:18 -0300

    update test example

--- backend/src/modules/users/services/CreateUserService.spec.ts
+++ backend/src/modules/users/services/CreateUserService.spec.ts
@@ -1,12 +1,18 @@
+import { container } from 'tsyringe';
+
+import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider';
 import AppError from '@shared/errors/AppError';
 
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import CacheUserService from './CacheUserService';
 import CreateUserService from './CreateUserService';
 
 let fakeUsersRepository: FakeUsersRepository;
 let fakeHashRepository: FakeHashProvider;
 let createUser: CreateUserService;
+let fakeCacheProvider: FakeCacheProvider;
+let cacheUser: CacheUserService;
 
 describe('CreateUser', () => {
   beforeEach(() => {
@@ -14,15 +20,27 @@ describe('CreateUser', () => {
     fakeHashRepository = new FakeHashProvider();
 
     createUser = new CreateUserService(fakeUsersRepository, fakeHashRepository);
+
+    fakeCacheProvider = new FakeCacheProvider();
+    cacheUser = new CacheUserService(fakeCacheProvider);
+
+    jest.spyOn(container, 'resolve').mockImplementation(() => {
+      return {
+        execute: () => jest.fn(),
+      };
+    });
   });
 
   it('should be able to create a new user', async () => {
+    const cacheUserExecute = jest.spyOn(cacheUser, 'execute');
+
     const user = await createUser.execute({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456',
     });
 
+    expect(cacheUserExecute).toHaveBeenCalled();
     expect(user).toHaveProperty('id');
   });
 
